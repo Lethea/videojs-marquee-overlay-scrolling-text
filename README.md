c0775e9a3b3795601f533cbd51ab627c5134d315
Emre Karatasoglu <emrek@boryazilim.com>
2017-10-12 12:52:55 +0300

    Marquee overlay with videojs

---
+++ README.md
@@ -0,0 +1,83 @@
+# videojs-marquee-overlay
+
+marquee plugin on video 
+
+Sample image : http://prntscr.com/gwflh5
+
+This will provide scrolling text on video both normal screen & full screen.
+Hls.js included
+Jquery included
+
+## Table of Contents
+
+<!-- START doctoc -->
+<!-- END doctoc -->
+## Installation
+
+```sh
+npm install --save videojs-marquee-overlay
+```
+
+## Usage
+
+To include videojs-marquee-overlay on your website or web application, use any of the following methods.
+
+### `<script>` Tag
+
+This is the simplest case. Get the script in whatever way you prefer and include the plugin _after_ you include [video.js][videojs], so that the `videojs` global is available.
+
+```html
+<script src="node_modules/video.js/dist/video.js"></script>
+<script src="dist/videojs-marquee-overlay.js"></script>
+<script src="node_modules/videojs-contrib-hls/dist/videojs-contrib-hls.js"></script>
+<script src="node_modules/jquery/dist/jquery.js"></script>
+<script src="node_modules/jquery.marquee/jquery.marquee.js"></script>
+<script>
+  var player = videojs('my-video');
+
+  player.marqueeOverlay({
+      contentOfMarquee: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+      position: "bottom",
+      direction: "left",
+      duration: 15000,
+      backgroundcolor: "red",
+      color: "#fefefe"
+    });
+</script>
+```
+
+### Browserify/CommonJS
+
+When using with Browserify, install videojs-marquee-overlay via npm and `require` the plugin as you would any other module.
+
+```js
+var videojs = require('video.js');
+
+// The actual plugin function is exported by this module, but it is also
+// attached to the `Player.prototype`; so, there is no need to assign it
+// to a variable.
+require('videojs-marquee-overlay');
+
+var player = videojs('my-video');
+
+player.marqueeOverlay();
+```
+
+### RequireJS/AMD
+
+When using with RequireJS (or another AMD library), get the script in whatever way you prefer and `require` the plugin as you normally would:
+
+```js
+require(['video.js', 'videojs-marquee-overlay'], function(videojs) {
+  var player = videojs('my-video');
+
+  player.marqueeOverlay();
+});
+```
+
+## License
+
+Apache-2.0. Copyright (c) Emre Karatasoglu (emre.karatasoglu@hotmail.com)
+
+
+[videojs]: http://videojs.com/
---
+++ src/plugin.js
@@ -0,0 +1,88 @@
+import videojs from 'video.js';
+import {version as VERSION} from '../package.json';
+
+// Default options for the plugin.
+const defaults = {
+  contentOfMarquee: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+  position: "bottom",
+  direction: "left",
+  duration: 15000,
+  backgroundcolor: "#dedede",
+  color: "#fefefe"
+};
+
+// Cross-compatibility for Video.js 5 and 6.
+const registerPlugin = videojs.registerPlugin || videojs.plugin;
+const dom = videojs.dom || videojs;
+
+/**
+ * Function to invoke when the player is ready.
+ *
+ * This is a great place for your plugin to initialize itself. When this
+ * function is called, the player will have its DOM and child components
+ * in place.
+ *
+ * @function onPlayerReady
+ * @param    {Player} player
+ *           A Video.js player object.
+ *
+ * @param    {Object} [options={}]
+ *           A plain object containing options for the plugin.
+ */
+const onPlayerReady = (player, options) => {
+  player.addClass('vjs-marquee-overlay');
+  const el = dom.createEl('div', {
+    className: `vjs-emre-marquee`
+  });
+  el.innerHTML = options.contentOfMarquee;
+  player.el().appendChild(el);
+  if (options.position == "bottom") {
+    el.style.bottom = 0;
+  } else {
+    el.style.top = 0;
+  }
+  el.style.backgroundColor=options.backgroundcolor;
+  el.style.color=options.color;
+  player.on('timeupdate', function () {
+    if (player.userActive()) {
+      el.style.bottom = "30px";
+    }else {
+      el.style.bottom = 0;
+    }
+  });
+  $(function () {
+    $('.vjs-emre-marquee').marquee({
+      duration: options.duration,
+      gap: 50,
+      delayBeforeStart: 0,
+      direction: options.direction,
+      duplicated: true
+    });
+  });
+};
+
+/**
+ * A video.js plugin.
+ *
+ * In the plugin function, the value of `this` is a video.js `Player`
+ * instance. You cannot rely on the player being in a "ready" state here,
+ * depending on how the plugin is invoked. This may or may not be important
+ * to you; if not, remove the wait for "ready"!
+ *
+ * @function marqueeOverlay
+ * @param    {Object} [options={}]
+ *           An object of options left to the plugin author to define.
+ */
+const marqueeOverlay = function (options) {
+  this.ready(() => {
+    onPlayerReady(this, videojs.mergeOptions(defaults, options));
+  });
+};
+
+// Register the plugin with video.js.
+registerPlugin('marqueeOverlay', marqueeOverlay);
+
+// Include the version number.
+marqueeOverlay.VERSION = VERSION;
+
+export default marqueeOverlay;
